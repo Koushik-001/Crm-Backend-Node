c6b71ff55986282757e0320e952514bb460a3f0f
koushik-001 <koushik.chv@gmail.com>
2025-02-20 11:57:51 +0530

    added swagger

--- index.js
+++ index.js
@@ -2,13 +2,41 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const loginRoutes = require('./src/route/loginRoute');
 const connectDB = require('./src/config/db');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const app = express();
-app.use(bodyParser.json());  
+app.use(bodyParser.json());
+
 connectDB
 
-app.use('/api', loginRoutes);  
+const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Node.js API',
+      version: '1.0.0',
+      description: 'API documentation for the Node.js application',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+      },
+    ],
+  },
+  apis: ['./src/route/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(swaggerOptions);
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+app.use('/api', loginRoutes);
+
 const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  console.log(`API documentation is available at http://localhost:${PORT}/api-docs`);
 });
--- src/route/loginRoute.js
+++ src/route/loginRoute.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const router = express.Router()
-const userloginController = require('../controller/loginController')
+const express = require('express');
+const router = express.Router();
+const userloginController = require('../controller/loginController');
 
+
+/**
+ * @swagger
+ * **/
 router.get('/users', userloginController.getUsers);
+
+/**
+ * @swagger
+ */
 router.post('/login', userloginController.createUser);
 
 module.exports = router;
